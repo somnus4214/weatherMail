c8037ea127404c8f9f7ef4dfe3710e474e390f7b
Somnus <chen1921460502@outlook.com>
2025-12-12 11:05:32 +0800

    feat: enhance email functionality with HTML support and base64 image encoding

--- src/mailserv.rs
+++ src/mailserv.rs
@@ -1,6 +1,10 @@
 use crate::staticconfig::get_smtp_config;
+use base64::{Engine as _, engine::general_purpose};
 use lettre::transport::smtp::authentication::Credentials;
-use lettre::{Message, SmtpTransport, Transport};
+use lettre::{Message, SmtpTransport, Transport, message::header};
+use std::fs;
+use tera::{Context, Tera};
+
 fn mail_send(mes: &str, target_mail: &str) -> Result<(), Box<dyn std::error::Error>> {
     let smtp_config = get_smtp_config();
     let email = Message::builder()
@@ -8,6 +12,47 @@ fn mail_send(mes: &str, target_mail: &str) -> Result<(), Box<dyn std::error::Err
         .to(target_mail.parse()?)
         .subject("Test from Rust")
         .body(String::from(mes))?;
+
+    let creds = Credentials::new(smtp_config.username.clone(), smtp_config.password.clone());
+
+    let mailer = SmtpTransport::starttls_relay(&smtp_config.server)?
+        .port(smtp_config.port)
+        .credentials(creds)
+        .build();
+
+    mailer.send(&email)?;
+    println!("邮件发送成功!");
+    Ok(())
+}
+
+fn mail_send_html(target_mail: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let smtp_config = get_smtp_config();
+
+    let image_data = fs::read("assets/icons/100-fill.svg")?;
+    let base64_image = general_purpose::STANDARD.encode(&image_data);
+    let data_uri = format!("data:image/svg+xml;base64,{}", base64_image);
+
+    let mut ctx = Context::new();
+    ctx.insert("date", "20251112");
+    ctx.insert("city", "chengdu");
+    ctx.insert("icon_url", &data_uri); // 使用 Base64 Data URI
+    ctx.insert("weather_desc", "sunny day");
+    ctx.insert("temp", "10");
+    ctx.insert("temp_max", "17");
+    ctx.insert("temp_min", "5");
+    ctx.insert("humidity", "80");
+    ctx.insert("wind_speed", "12");
+    ctx.insert("suggestion", "example_txt");
+    ctx.insert("bot_name", "Rust Weather Bot");
+
+    let tera = Tera::new("templates/*.tera")?;
+    let html = tera.render("weatheremail.html.tera", &ctx)?;
+    let email = Message::builder()
+        .from(smtp_config.from.parse()?)
+        .to(target_mail.parse()?)
+        .subject("Test from Rust")
+        .header(header::ContentType::TEXT_HTML)
+        .body(html)?;
 
     let creds = Credentials::new(smtp_config.username.clone(), smtp_config.password.clone());
 
@@ -32,6 +77,14 @@ mod test {
         match mail_send(mes, test_mail) {
             Ok(_) => println!("测试通过"),
             Err(e) => eprintln!("发送失败: {}", e),
+        }
+    }
+    #[test]
+    fn test_html_mail_send() {
+        let test_mail = "chen1921460502@outlook.com";
+        match mail_send_html(test_mail) {
+            Ok(_) => println!("测试通过"),
+            Err(e) => eprintln!("发送失败：{}", e),
         }
     }
 }
